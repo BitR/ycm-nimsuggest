514fafe49e019b07e9cc8dd985eb23c30248cd12
BitR <bitr@bitsaw.com>
2015-06-04 22:47:33 +0200

    Queries are now handled with threads, making completions take .05 seconds after nimsuggest has loaded a file. Added a __main__ to test the completer, until proper unit tests are added.

--- nimsuggest_completer.py
+++ nimsuggest_completer.py
@@ -2,9 +2,14 @@
 
 import os
 import sys
+import io
 import tempfile
 import logging
 import itertools
+import time
+import traceback
+from threading import Thread
+from Queue import Queue, Empty
 
 from subprocess import Popen, PIPE
 from ycmd.completers.completer import Completer
@@ -25,10 +30,14 @@ def info(msg):
 def debug(msg):
     log(logging.DEBUG, msg)
 
+COMPLETION_TIMEOUT=0.05
+BOOT_TIMEOUT=2
+
 class NimsuggestCompleter(Completer):
     def __init__(self, user_options):
         super(NimsuggestCompleter, self).__init__(user_options)
         self._binary = utils.PathToFirstExistingExecutable(['nimsuggest'])
+        self._dataqueue = Queue()
 
         if not self._binary:
             msg = "Couldn't find nimsuggest binary. Is it in the path?"
@@ -39,6 +48,60 @@ class NimsuggestCompleter(Completer):
 
     def SupportedFiletypes(self):
         return set(['nim'])
+
+    def _RunProc(self, args):
+        try:
+            self._proc = Popen([self._binary] + args, executable = self._binary, stdin = PIPE, stdout = PIPE, stderr = PIPE, shell=True)
+        except:
+            error(traceback.format_exc())
+            return
+
+        try:
+            while not self._proc.poll():
+                data = self._proc.stdout.readline()
+                self._dataqueue.put(data)
+        finally:
+            if self._proc.returncode != None and self._proc.returncode != 0:
+                error('Nimsuggest retcode %d: %s' % (self._proc.returncode, data))
+                return []
+
+            try:
+                self._proc.stdin.write('quit\n')
+                self._proc.stdin.flush()
+            except:
+                pass
+
+    def OnFileReadyToParse(self, request_data):
+        filepath = request_data['filepath']
+        args = ['--stdin', filepath]
+
+        if hasattr(self, '_proc'):
+            if not self._proc.poll():
+                warning('Nimsuggest has exited with code %d' % self._proc.returncode)
+                return
+        self._readthread = Thread(target=self._RunProc, args=[args])
+        self._readthread.daemon = True
+        self._readthread.start()
+
+    def _Cmd(self, data):
+        if not hasattr(self, '_proc'):
+            # Seems the thread isn't quite ready yet
+            return ''
+        self._proc.stdin.write(data)
+        self._proc.stdin.flush()
+        output = ''
+        found_cmd = False
+        while True:
+            try:
+                d = self._dataqueue.get(timeout=(COMPLETION_TIMEOUT if found_cmd else BOOT_TIMEOUT))
+                if data in d:
+                    output = ''
+                    found_cmd = True
+                    continue
+                output += d
+            except Empty:
+                break
+        return output
 
     def ShouldUseNowInner(self, request_data):
         return len(self.ComputeCandidatesInner(request_data)) > 0
@@ -52,25 +115,18 @@ class NimsuggestCompleter(Completer):
         return self._Suggest(filepath, linenum, colnum, contents)
 
     def _Suggest(self, filename, linenum, column, contents):
-        args = ['--stdin', filename]
-        p = Popen(args, executable = self._binary, stdin = PIPE, stdout = PIPE, stderr = PIPE, shell=True)
         dirtyfile = None
         if contents:
             dirtyfile = tempfile.mkstemp()[1]
             open(dirtyfile, 'w').write(contents)
-        cmd = 'sug %s%s:%d:%d\nquit\n' % (filename, ';' + 
+        cmd = 'sug %s%s:%d:%d\n' % (filename, ';' + 
                 dirtyfile if dirtyfile else '', linenum, column)
         try:
-            data, err = p.communicate(cmd)
-
-            if p.returncode != 0:
-                error('Nimsuggest retcode %d: %s' % (p.returncode, data))
-                return []
+            data = self._Cmd(cmd)
 
             return [self._CreateCompletionData(line,
                         contents.splitlines()[linenum-1] if contents and len(contents) >= linenum - 1 else None)
-                    for line in list(itertools.dropwhile(
-                        lambda l: not l.startswith('>'), data.splitlines()))[1:]
+                    for line in data.splitlines()
                     if line.startswith('sug')]
         except KeyboardInterrupt:
             pass
@@ -99,7 +155,7 @@ if __name__ == "__main__":
                 'min_num_of_chars_for_completion': 1,
                 'auto_trigger': None
                 })
-    tmpfile = tempfile.mkstemp()[1]
+    tmpfile = tempfile.mkstemp('.nim')[1]
     contents = """import math
 
 a = open('/dev/zero', 'r')
@@ -119,8 +175,9 @@ a.
                 }
             }
         }
+        compl.OnFileReadyToParse(request_data)
+        time.sleep(.5)
         compl.ComputeCandidatesInner(request_data)
         compl.ComputeCandidatesInner(request_data)
-        #print compl.ComputeCandidatesInner(request_data)
     finally:
         os.unlink(tmpfile)
